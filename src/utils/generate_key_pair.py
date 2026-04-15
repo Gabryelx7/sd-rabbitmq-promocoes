6904d707de4ed33c6f744aab6b16c526a0329d0f
Gabryelx7 <gabrielmsd@live.com>
2026-04-15 15:31:11 -0300

    fix: generate all keys at once

--- src/utils/generate_key_pair.py
+++ src/utils/generate_key_pair.py
@@ -1,28 +1,30 @@
 from cryptography.hazmat.primitives.asymmetric import ed25519
 from cryptography.hazmat.primitives import serialization
 
-KEY_NAME = "promocao"
-PRIVATE_KEY_FILE = f"keys/{KEY_NAME}_private_key.pem"
-PUBLIC_KEY_FILE = f"keys/{KEY_NAME}_public_key.pem"
+KEYS = ["promocao", "ranking", "gateway", "notificacao"]
 
-private_key = ed25519.Ed25519PrivateKey.generate()
-public_key = private_key.public_key()
+for key in KEYS:
+    PRIVATE_KEY_FILE = f"keys/{key}_private_key.pem"
+    PUBLIC_KEY_FILE = f"keys/{key}_public_key.pem"
 
-private_pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-)
+    private_key = ed25519.Ed25519PrivateKey.generate()
+    public_key = private_key.public_key()
 
-with open(PRIVATE_KEY_FILE, "wb") as f:
-    f.write(private_pem)
-print(f"Chave privada salva em: {PRIVATE_KEY_FILE}")
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
 
-public_pem = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
+    with open(PRIVATE_KEY_FILE, "wb") as f:
+        f.write(private_pem)
+    print(f"Chave privada salva em: {PRIVATE_KEY_FILE}")
 
-with open(PUBLIC_KEY_FILE, "wb") as f:
-    f.write(public_pem)
-print(f"Chave pública salva em: {PUBLIC_KEY_FILE}")
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    with open(PUBLIC_KEY_FILE, "wb") as f:
+        f.write(public_pem)
+    print(f"Chave pública salva em: {PUBLIC_KEY_FILE}")
