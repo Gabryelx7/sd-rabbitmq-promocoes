7bd73fb098a984a31221009e268b56b50b855c74
Gabryelx7 <gabrielmsd@live.com>
2026-04-14 21:33:23 -0300

    feat: add script to generate the assymetric keys

---
+++ src/utils/generate_key_pair.py
@@ -0,0 +1,28 @@
+from cryptography.hazmat.primitives.asymmetric import ed25519
+from cryptography.hazmat.primitives import serialization
+
+KEY_NAME = "notificacao"
+PRIVATE_KEY_FILE = f"keys/{KEY_NAME}_private.pem"
+PUBLIC_KEY_FILE = f"keys/{KEY_NAME}_public.pem"
+
+private_key = ed25519.Ed25519PrivateKey.generate()
+public_key = private_key.public_key()
+
+private_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+with open(PRIVATE_KEY_FILE, "wb") as f:
+    f.write(private_pem)
+print(f"Chave privada salva em: {PRIVATE_KEY_FILE}")
+
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+with open(PUBLIC_KEY_FILE, "wb") as f:
+    f.write(public_pem)
+print(f"Chave pública salva em: {PUBLIC_KEY_FILE}")
